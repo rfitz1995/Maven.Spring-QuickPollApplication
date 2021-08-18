bcd93d0cf9c9f139f59e9305e97e62a233df6752
rfitz1995 <rfitz@udel.edu>
2021-08-18 13:19:53 -0400

    poll controller tested in postman

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class PollController {
@@ -23,6 +25,31 @@ public class PollController {
         Iterable<Poll> allPolls = repo.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
+    @PostMapping(value="/polls")
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        poll = repo.save(poll);
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+        return new ResponseEntity<>(newPollUri, HttpStatus.CREATED);
+    }
 
-
+    @GetMapping(value="/polls/{pollId}")
+    public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+        Poll p = repo.findOne(pollId);
+        return new ResponseEntity<> (p, HttpStatus.OK);
+    }
+    @PutMapping(value="/polls/{pollId}")
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+        // Save the entity
+        Poll p = repo.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @DeleteMapping(value="/polls/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        repo.delete(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
